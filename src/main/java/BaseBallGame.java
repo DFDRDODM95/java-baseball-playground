682437ecd96428f191089254cd366ff73745c2ff
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-02-14 08:28:00 +0900

    refactor() remove else statement

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -19,29 +19,33 @@ public class BaseBallGame {
 
     }
     static void guess(String ans) {
-        while (true) {
+        String input;
+        do {
             System.out.println("숫자를 입력해 주세요.");
-            String input = scanner.nextLine();
-            if (input.length() != 3 || !isNum(input)) {
-                System.out.println("입력이 잘못되었습니다.");
-                continue;
-            }
-            int strike = 0;
-            int ball = 0;
-            for (int i = 0; i < 3; i++) {
-                if (ans.charAt(i) == input.charAt(i)) strike++;
-                else if (ans.contains(input.substring(i, i + 1))) ball++;
-            }
-            if (strike == 3) {
-                break;
-            }
-            else if (strike > 0){
-                System.out.println(ball + "볼 " + strike + "스트라이크");
-            }
-            else {
-                System.out.println(ball + "볼");
-            }
+            input = scanner.nextLine();
+        } while (count(ans, input));
+    }
+
+    static boolean count(String ans, String input) {
+        String rst = "입력이 잘못되었습니다.";
+        boolean sw = false;
+        if (input.length() != 3 || !isNum(input)) sw = true;
+        if (sw) System.out.println(rst);
+        if (sw) return true;
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            int flag = 0;
+            if (ans.contains(input.substring(i, i + 1))) flag++;
+            if (ans.charAt(i) == input.charAt(i)) flag++;
+            if (flag == 2) strike++;
+            if (flag == 1) ball++;
         }
+        if (strike == 3) return false;
+        if (ball > 0) rst = ball + "볼";
+        if (strike > 0) rst = ball + "볼 " + strike + "스트라이크";
+        System.out.println(rst);
+        return true;
     }
 
     static boolean gameEnd() {
@@ -50,21 +54,15 @@ public class BaseBallGame {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String input = scanner.nextLine();
             if (Objects.equals(input, "2")) return false;
-            else if (Objects.equals(input, "1")) return true;
-            else {
-                System.out.println("입력이 잘못되었습니다.");
-            }
+            if (Objects.equals(input, "1")) return true;
+            System.out.println("입력이 잘못되었습니다.");
         }
     }
 
     static boolean isNum(String text) {
         ArrayList<String> numbs = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
-            numbs.add(String.valueOf(i));
-        }
-        for (String s : text.split("")) {
-            if (!numbs.contains(s)) return false;
-        }
+        for (int i = 1; i < 10; i++) numbs.add(String.valueOf(i));
+        for (String s : text.split("")) if (!numbs.contains(s)) return false;
         return true;
     }
 }
